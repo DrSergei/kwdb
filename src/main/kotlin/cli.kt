c1aaa6768dcecfaa27d7191bf6c3778c95b5a5b3
DrSergei <serzhdruzhok@gmail.com>
2021-10-10 10:23:25 +0300

    The functionality of the command line parser has been split, calls to input functions have been moved to main.kt.

--- src/main/kotlin/cli.kt
+++ src/main/kotlin/cli.kt
@@ -6,7 +6,6 @@
 package cli
 
 // Импорт.
-import frontend.*
 import kotlinx.cli.*
 import style.*
 import style.Message.*
@@ -23,21 +22,29 @@ enum class Mode {
 }
 
 /**
+ * Аргументы командной строки.
+ *
+ * Входной файл(если пустой, то консоль) и режим работы.
+ */
+data class ArgsCLI(val input: String, val mode: Mode)
+
+/**
  * Служебная функция.
  *
  * Обрабатывает аргументы командной строки и выбирает сценарий работы.
  */
-fun prepareArgs(args: Array<String>) {
+fun prepareArgs(args: Array<String>) : ArgsCLI {
     try {
         val parser = ArgParser("kvdb")
         val input by parser.option(ArgType.String, shortName = "i", description = "Input file(txt file)")
         val mode by parser.option(ArgType.Choice<Mode>(), shortName = "m", description = "Operating mode").default(Mode.READ)
         parser.parse(args)
-        if (input != null)
-            inputFile(input.toString(), mode)
+        if (input == null)
+            return ArgsCLI("", mode)
         else
-            inputConsole(mode)
+            return ArgsCLI(input!!, mode) // есть проверка на null, безопасный вызов
     } catch (e: Exception) {
         println(report(INVALID_ARGUMENTS))
+        throw e
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,17 @@
 // Импорт.
 
 import cli.*
+import frontend.*
 import kotlin.system.*
 
 fun main(args: Array<String>) {
     try {
         val time = measureTimeMillis { // замер времени работы программы
-            prepareArgs(args)
+            val argsCLI = prepareArgs(args)
+            if (argsCLI.input.isEmpty())
+                inputConsole(argsCLI.mode)
+            else
+                inputFile(argsCLI.input, argsCLI.mode)
         }
         println("The session was ${time / 1000 /60} min.")
     } catch (e : Exception) {
